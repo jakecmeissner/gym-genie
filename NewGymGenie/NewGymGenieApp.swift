e57783c32714f3fa698af5cc0ed7a165c55da355
jakecmeissner <59347388+jakecmeissner@users.noreply.github.com>
2023-04-11 16:48:44 -0400

    Initial Commit

---
+++ NewGymGenie/NewGymGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewGymGenieApp.swift
+//  NewGymGenie
+//
+//  Created by Jake Meissner on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NewGymGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
